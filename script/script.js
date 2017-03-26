abf6ce7cfe1e35b1866c9d239c75d07a0262f63a
Sahib <huseynov.sahib@yahoo.com>
2017-03-27 01:43:45 +0400

    fifth

--- script/script.js
+++ script/script.js
@@ -3,70 +3,113 @@ var placeholder = document.querySelector('#searchPlaceholder');
 var trick = document.querySelector('.searchTrick');
 var searchInput = document.getElementsByClassName('searchInput')[0];
 var span = document.getElementsByClassName('exVal');
-
 function newVal(val){
 	searchInput.value = val.innerText;
 }
 
-searchInput.addEventListener('click',function(){
+
+window.onclick=function(event){
+	if (event.target!=searchInput) {
+		placeholder.style.top=20 + '%';
+		placeholder.style.left=1 + '%';
+		placeholder.style.fontSize="27px";
+		trick.style.display = 'none';
+		placeholder.setAttribute('class','')
+	}else {
+		searchInput.addEventListener('click',function(){
 	trick.style.display = 'block';
 	placeholder.setAttribute('class','focus')
+	placeholder.style.top=0;
+	placeholder.style.left=0;
+	placeholder.style.fontSize="20px";
 })
-// searchInput.addEventListener('blur',function(){
-// 	trick.style.display = 'none';
-// 	placeholder.setAttribute('class','')
-// })
+	}
+}
 
+/*slider*/
 
 
 
 var sliderArr = ['images/500x230_2-1.jpg','images/500x230-1.jpg','images/emek-kredit-1.jpg'];
 var i=0;
+var bullets = document.getElementsByClassName('bullets');
 var sliderBg = document.querySelector('.slider');
 sliderBg.src = sliderArr[0];
-function slider(){
+
+var slideNum = 1;
+slider(slideNum);
+function prevNext(n) {
+  slider(slideNum = n);
+}
+
+function slider(n){
 	i++;
 	if (i==sliderArr.length) {
 		i=0
 	}
 	sliderBg.src = sliderArr[i];
+	// for (var x = 0; x <sliderArr.length; x++) {
+	// 	bullets[x].className=bullets[x].className.replace(' bulletSelected','');
+	// }
+	// bullets[slideNum-1].className+='bulletSelected';
+	
 }setInterval(slider,3000);
 
-/*internet bank side*/
+/*right side functions*/
 var zoomMain = document.querySelector('#main');
-function close(btn){
-	setTimeout(function(){
-		divs.style.display = 'none';
-		setTimeout(function(){
-			divs.style.right = "-50%";
-		},50)
-	},50)
-	zoomMain.setAttribute('class','zoom_in');
-	
-	
-	// btn.style.display = 'none';
-	// zoomMain.setAttribute('class','');
-	// changeBg.style.display ='none';
+
+var contactBtn=document.querySelector('.contactBtn');
+var onlineBank=document.querySelector('.onlineBank');
+var internetBank=document.querySelector('.internetBank');
+var writeUs=document.querySelector('.writeUs');
+
+var closeDivfirst=document.querySelector(".closeDivfirst");
+var closeDivsecond=document.querySelector(".closeDivsecond");
+var closeDivthird=document.querySelector(".closeDivthird");
+
+writeUs.onclick=function(){	
+	document.querySelector('.writeUsForm').style.visibility='visible';
+	document.querySelector('.writeUsForm').style.right='-4%';
+	document.querySelector('.contactTypes').style.left='-100%';
+	document.querySelector('.leftArrow').style.left='0';
+	document.querySelector('.conCapP').style.left='7px';
 }
-function showDivs(divs){
-	setTimeout(function(){
-		divs.style.display = 'block';
-		setTimeout(function(){
-			divs.style.right = 0;
-		},50)
-	},50)
-	
+document.querySelector('.leftArrow').onclick=function(){
+	document.querySelector('.writeUsForm').style.visibility='hidden';
+	document.querySelector('.writeUsForm').style.right='-100vh';
+	document.querySelector('.contactTypes').style.left='0';
+	document.querySelector('.leftArrow').style.left='-20%';
+	document.querySelector('.conCapP').style.left='0';
+}
+
+contactBtn.onclick=function(){
 	zoomMain.setAttribute('class','zoom_out');
+	document.querySelector('#contactUs').style.right='0px';
+}
+onlineBank.onclick=function(){
+	zoomMain.setAttribute('class','zoom_out');
+	document.querySelector('#bankOnline').style.right='0px';
+}
+internetBank.onclick=function(){
+	zoomMain.setAttribute('class','zoom_out');
+	document.querySelector('#internetBank').style.right='0px';
 }
 
-// document.window.addEventListener('dblclick',function(divs){
-// 	if (divs.style.display == 'block') {
-// 		divs.style.display == 'none';
-// 	}
-// 	zoomMain.setAttribute('class','');
-	
-// })
+closeDivthird.onclick=function(){
+	zoomMain.setAttribute('class','zoom_in');
+	document.querySelector('#bankOnline').style.right='-50%';
+}
+closeDivsecond.onclick=function(){
+	zoomMain.setAttribute('class','zoom_in');
+	document.querySelector('#contactUs').style.right='-50%';
+}
+closeDivfirst.onclick=function(){
+	zoomMain.setAttribute('class','zoom_in');
+	document.querySelector('#internetBank').style.right='-50%';
+}
 
+
+/*farmazon*/
 var changeCapArr = ['Fərdi','Korporativ'];
 var individualListArr =['Bank 24/7', 'Əmanətlər','Elektron xidmətlər','Cari hesab','Kreditlər','Ödəniş kartları','Köçürmələr','Kampaniyalar'];
 var corporativeListArr=['Elektron bankçılıq', 'Sənədli əməliyyatlar','Ödəniş kartları','Hesablar','Biznes kreditləri','Əmanətlər','Köçürmələr','Bizimlə əlaqə'];
@@ -84,6 +127,8 @@ var overlay = document.createElement('div');
 var h2 = document.querySelector('.hTitle');
 var hLink = document.querySelectorAll('.hLink');
 
+
+
 individualBtn.addEventListener('click', function(){
 	 
 	setTimeout(function(){
@@ -92,8 +137,8 @@ individualBtn.addEventListener('click', function(){
 		middleSection.style.top = '-100vh';
 	setTimeout(function(){
 		middleSection.style.display = 'none';
-	},50)
-	},50)},50);
+	},30)
+	},30)},30);
 		setTimeout(function(){myFun()
 			setTimeout(function(){
 				h2.innerHTML=changeCapArr[0];
@@ -107,16 +152,22 @@ individualBtn.addEventListener('click', function(){
 
 			},30)
 			},30)
-	
+		if (changeBg.style.visibility!='visible') {
+		setTimeout(myFun(),30)
+		}else {
+			window.myFun = null;
+		}
+		
 })
 function myFun(){
-
+	
 	var n = 'polygon(93% 0, 100% 0, 100% 100%, 95% 100%)';
 	var m = 'polygon(89% 0, 100% 0, 100% 100%, 75% 100%)';
 	var k = "polygon(75% 0, 100% 0, 100% 100%, 55% 100%)";
 	var l = 'polygon(69% 0, 100% 0, 100% 100%, 25% 100%)';
 	var d = 'polygon(65% 0, 100% 0, 100% 100%, 15% 100%)';
 	
+	changeBg.style.visibility='visible';
 	setTimeout(function(){
 		changeBg.style.clipPath = n;
 		setTimeout(function(){
@@ -132,11 +183,15 @@ function myFun(){
 			},60)
 		},40)
 	},20)
-	changeBg.style.display = 'block';
+	
 }
 corporateBtn.addEventListener('click', function(){
-	setTimeout(myFun(),30)
-
+	if (changeBg.style.visibility!='visible') {
+		setTimeout(myFun(),30)
+	}else {
+		window.myFun = null;
+	}
+	
 	setTimeout(function(){
 	middleSection.style.top = '-50vh';
 	setTimeout(function(){
@@ -156,3 +211,13 @@ corporateBtn.addEventListener('click', function(){
 	corporateBtn.setAttribute('class','active');
 	individualBtn.setAttribute('class','');
 })
+
+
+/*phone mask*/
+var phone_mask=document.getElementsByClassName('phoneMask')[0];
+phone_mask.defaultValue='+994 00 000-00-00';
+if (phone_mask.value.length == 2) {
+	phone_mask.value = "";
+}else if (phone_mask.length==3) {
+	phone_mask.innerHTML ="-";
+}
---
+++ script/scriptd.js
@@ -0,0 +1,138 @@
+var depoLeft = document.getElementsByClassName('depoLeft')[0];
+
+window.onscroll = function() {bodyBg()};
+
+function bodyBg() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+		document.body.style.background='#3d62ac';
+		depoLeft.style.position='fixed';
+    } else {
+		document.body.style.background='#3d62ac';
+		depoLeft.style.position='relative';
+    }
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
+    	document.querySelector('.conCap').style.background='#8da2ce';
+    	document.querySelector('.subCap').style.background='';
+    	document.querySelector('.ratesCap').style.background='';
+    	document.querySelector('.calCap').style.background='';
+    }
+    if (document.body.scrollTop >700 || document.documentElement.scrollTop > 700){
+    	document.querySelector('.subCap').style.background='#8da2ce';
+    	document.querySelector('.conCap').style.background='';
+    	document.querySelector('.ratesCap').style.background='';
+    	document.querySelector('.calCap').style.background='';
+    }
+     if (document.body.scrollTop >900 || document.documentElement.scrollTop > 900){
+    	document.querySelector('.ratesCap').style.background='#8da2ce';
+    	document.querySelector('.conCap').style.background='';
+    	document.querySelector('.subCap').style.background='';
+    	document.querySelector('.calCap').style.background='';
+    }
+     if (document.body.scrollTop >1400 || document.documentElement.scrollTop > 1400){
+    	document.querySelector('.calCap').style.background='#8da2ce';
+    	document.querySelector('.conCap').style.background='';
+    	document.querySelector('.subCap').style.background='';
+    	document.querySelector('.ratesCap').style.background='';
+    }
+}
+
+
+
+
+
+var amountInput=document.querySelector('.amountInput');
+var spanAmount = document.querySelector('.amount');
+
+var periodInput=document.querySelector('.periodInput');
+var spanPeriod=document.querySelector('.period');
+
+
+var spanPerTotal = document.querySelector('.percentTotal');
+var spanPerMon = document.querySelector('.percentMonth');
+
+var rateDeg=document.querySelector('.rateDeg');
+var azn=document.querySelector('#azn');
+var usd =document.querySelector('#usd');
+var curVal=document.querySelector('.curVal');
+
+azn.onclick=function(){
+	periodInput.disabled=false;
+	document.querySelectorAll('.curVal')[0].innerHTML = 'azn';
+	document.querySelectorAll('.curVal')[1].innerHTML = 'azn';
+	
+}
+usd.onclick=function(){
+
+var x = document.getElementById("calcPayment").value;
+
+	periodInput.disabled=true;
+
+
+	if (x=='month') {
+		rateDeg.innerHTML='3%';
+	}else {
+		rateDeg.innerHTML='3.25%';
+	}
+	document.querySelectorAll('.curVal')[0].innerHTML = 'usd';
+	document.querySelectorAll('.curVal')[1].innerHTML = 'usd';
+}
+
+function amountRange(){
+	spanAmount.innerHTML=amountInput.value;
+}
+
+function periodRange(){
+
+	var x = document.getElementById("calcPayment").value;
+
+	if (periodInput.value==0) {
+		spanPeriod.innerHTML=12;
+	} else if (periodInput.value==1) {
+		spanPeriod.innerHTML=24;
+	}else{
+		spanPeriod.innerHTML=36;
+	}
+	if (spanPeriod.innerText==24 || spanPeriod.innerText==36){
+		rateDeg.innerHTML='12%';
+	}else {
+		rateDeg.innerHTML='11%';
+	}
+	if (x!='month' && spanPeriod.innerText==12) {
+		rateDeg.innerHTML='12%';
+	}else if (x!='month' && (spanPeriod.innerText==24 || spanPeriod.innerText==36)) {
+		rateDeg.innerHTML='13%';
+	}
+}
+
+function calcCredit(){
+	var x = document.getElementById("calcPayment").value;
+	if (x=='month' && spanPeriod.innerText==12) {
+		spanPerTotal.innerHTML=(amountInput.value*11)/100;
+		spanPerMon.innerHTML=(amountInput.value*0.92)/100;
+		
+	}else if (x=='month' && (spanPeriod.innerText==24 || spanPeriod.innerText==36)) {
+		spanPerTotal.innerHTML=(amountInput.value*12)/100;
+		spanPerMon.innerHTML=(amountInput.value*1)/100;
+
+	}else if (x!='month' && spanPeriod.innerText==12) {
+		spanPerTotal.innerHTML=(amountInput.value*12)/100;
+		spanPerMon.innerHTML=(amountInput.value*12)/100;
+	}else if (x!='month' && (spanPeriod.innerText==24 || spanPeriod.innerText==36)) {
+		spanPerTotal.innerHTML=(amountInput.value*13)/100;
+		spanPerMon.innerHTML=(amountInput.value*13)/100;
+	}
+}
+
+function calcUsd(){
+	var x = document.getElementById("calcPayment").value;
+	if (periodInput.disabled=true) {
+		if (x=='month'){
+		spanPerTotal.innerHTML=(amountInput.value*3)/100;
+		spanPerMon.innerHTML=(amountInput.value*0.25)/100;
+		
+	}else{
+		spanPerTotal.innerHTML=(amountInput.value*3.25)/100;
+		spanPerMon.innerHTML=(amountInput.value*3.25)/100;
+	}
+}
+}
